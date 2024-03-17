63bfc313926182b11e29a868c286475ac25b8218
Kajal Deore <137670233+KajalDeore04@users.noreply.github.com>
2024-03-17 17:16:15 +0530

    feedback.js

--- feedback.js
+++ feedback.js
@@ -1,8 +1,4 @@
 document.getElementById("feedbackForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent default form submission
-
-    // Here you can handle the form submission, e.g., send data to the server
-
-    // Show a popup message after form submission
+    event.preventDefault(); 
     alert("Feedback submitted successfully!");
 });
